b34127a8c64a4106b9d5e0f1fc4eeb0645e0650d
nabish00 <nabish00@gmail.com>
2018-10-28 23:20:02 +0500

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,110 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using AspWebapipractiseid4_27102018.Data;
+using AspWebapipractiseid4_27102018.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace AspWebapipractiseid4_27102018
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public IHostingEnvironment Environment { get; }
+
+        public Startup(IConfiguration configuration, IHostingEnvironment environment)
+        {
+            Configuration = configuration;
+            Environment = environment;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connectionString = Configuration.GetConnectionString("DefaultConnection");
+
+            var migrationAssembly = typeof(Startup).GetType().Assembly.GetName().Name;
+
+            
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseMySql(connectionString));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddMvc();
+
+            services.Configure<IISOptions>(iis =>
+            {
+                iis.AuthenticationDisplayName = "Windows";
+                iis.AutomaticAuthentication = false;
+            });
+
+            var builder = services.AddIdentityServer(options =>
+            {
+                //options.Events.RaiseErrorEvents = true;
+                //options.Events.RaiseInformationEvents = true;
+                //options.Events.RaiseFailureEvents = true;
+                //options.Events.RaiseSuccessEvents = true;
+            })
+            .AddConfigurationStore(configDb =>
+            {
+                configDb.ConfigureDbContext = db => db.UseMySql(connectionString,
+                                                                sql => sql.MigrationsAssembly("AspWebapipractiseid4_27102018"));
+
+            })
+            .AddOperationalStore(operationalDb =>
+            {
+                operationalDb.ConfigureDbContext = db => db.UseMySql(connectionString,
+                                                                sql => sql.MigrationsAssembly("AspWebapipractiseid4_27102018"));
+
+            }).AddAspNetIdentity<ApplicationUser>();
+                //.AddInMemoryIdentityResources(Config.GetIdentityResources())
+                //.AddInMemoryApiResources(Config.GetApis())
+                //.AddInMemoryClients(Config.GetClients())
+                //.AddAspNetIdentity<ApplicationUser>()
+                //.AddAspNetIdentity<ApplicationUser>();
+
+            if (Environment.IsDevelopment())
+            {
+                builder.AddDeveloperSigningCredential();
+            }
+            else
+            {
+                throw new Exception("need to configure key material");
+            }
+
+            services.AddAuthentication()
+                .AddGoogle(options =>
+                {
+                    options.ClientId = "708996912208-9m4dkjb5hscn7cjrn5u0r4tbgkbj1fko.apps.googleusercontent.com";
+                    options.ClientSecret = "wdfPY6t8H8cecgjlxud__4Gh";
+                });
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            if (Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseIdentityServer();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
